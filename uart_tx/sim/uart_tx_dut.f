1597da95c631b6a88a2a0205fc0dc6d922855833
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-12-03 02:52:05 +0200

    Delete uart_tx/sim directory

--- uart_tx/sim/uart_tx_dut.f
+++
@@ -1,8 +0,0 @@
-E:/UVM/UART/uart_tx/rtl/mux.v
-E:/UVM/UART/uart_tx/rtl/parity_calc.v
-E:/UVM/UART/uart_tx/rtl/serializer.v
-E:/UVM/UART/uart_tx/rtl/tx_fsm.v
-E:/UVM/UART/uart_tx/rtl/uart_tx.v
-
-
-+incdir+E:/UVM/UART/uart_tx/rtl/
