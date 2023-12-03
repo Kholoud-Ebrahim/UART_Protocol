65d302f3da39056cfd8180eecc8743a63a003abf
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-12-03 03:19:18 +0200

    Delete uart_rx directory

--- uart_rx/tb/uart_rx_intf.sv
+++
@@ -1,8 +0,0 @@
-interface uart_rx_intf #(parameter DWIDTH = 8, PWIDTH =6)(input clk, rst);   
-    logic s_data_rx;                           //Receiver input
-    logic parity_type_rx, parity_en_rx;
-    logic [PWIDTH-1:0]prescale_rx;
-
-    logic [DWIDTH-1:0]p_data_rx;               //Receiver output
-    logic data_valid_rx;
-endinterface :uart_rx_intf
